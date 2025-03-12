017ec554c5aad29fdc049f9af2b36b34cca55c84
elliotewpti <dmitriymarlowpierrebourne@gmail.com>
2025-03-12 10:17:22 +0300

    feat(styles): Add CollectionList styles

--- src/components/CollectionList.svelte
+++ src/components/CollectionList.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
 	import type { ICollection } from '$lib/icontract';
+	import CollectionCard from './CollectionCard.svelte';
 
 	const { collections }: { collections: ICollection[] } = $props();
-	console.log(collections);
 </script>
 
-<div>
-	{#each collections as collection}
-		<div>
-			<span>{collection.name}</span>
-		</div>
-	{/each}
+<div class="flex items-center justify-center">
+	{#if collections.length > 0}
+		{#each collections as collection}
+			<div class="inline-grid grid-cols-3 gap-4">
+				<CollectionCard {collection} />
+			</div>
+		{/each}
+	{:else}
+		<span class="mx-auto text-2xl font-bold text-white">You haven't collections</span>
+	{/if}
 </div>
