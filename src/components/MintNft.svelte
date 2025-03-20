ba5f08a771e4b57dea5a69c22fff61f442bc68a4
elliotewpti <dmitriymarlowpierrebourne@gmail.com>
2025-03-20 10:05:09 +0300

    feat(MintNft): Create files upload

--- src/components/MintNft.svelte
+++ src/components/MintNft.svelte
@@ -2,10 +2,20 @@
 	import { contract, type TAccountInfo } from '$lib/web3';
 	import Button from './Button.svelte';
 	import notifications from '$lib/notifications';
+	import { Api } from '$lib/api';
 
 	const { account }: { account: TAccountInfo } = $props();
 
 	let nftName = $state('');
+	let file: File | null = $state(null);
+
+	function onFileChange(event: Event) {
+		const input = event.currentTarget as HTMLInputElement;
+
+		if (input.files && input.files.length > 0) {
+			file = input.files[0];
+		}
+	}
 
 	async function onMint() {
 		try {
@@ -13,6 +23,21 @@
 				from: account.address,
 				gas: '6721965'
 			});
+
+			if (file) {
+				const formData = new FormData();
+				formData.append('file', file);
+				formData.append('name', nftName);
+				formData.append('owner', account.address);
+
+				const opts = {
+					method: 'POST',
+					route: 'api/nft/save',
+					body: formData
+				};
+
+				await Api.fetchApi(opts);
+			}
 
 			notifications.add('success', 'NFT minted', `Tx hash ${txHash.transactionHash}`);
 		} catch (err) {
@@ -25,6 +50,7 @@
 </script>
 
 <div class="mx-auto flex max-w-md flex-col gap-4">
+	<input type="file" onchange={onFileChange} />
 	<input
 		type="text"
 		placeholder="NFT Name"
