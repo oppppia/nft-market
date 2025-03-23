c8881ac7efd5dee60920f73afde6a5c450637dad
elliotewpti <dmitriymarlowpierrebourne@gmail.com>
2025-03-23 23:44:59 +0300

    feat(NftCard): Add place/buy dialogs, add function for get nft image, add refreshing nfts list after onPlace/onBuy

--- src/components/NftCard.svelte
+++ src/components/NftCard.svelte
@@ -1,45 +1,76 @@
 <script lang="ts">
 	import type { INft } from '$lib/icontract';
 	import Button from './Button.svelte';
-	import nftIcon from '../images/nft.png';
 	import notificationsStore from '$lib/notifications';
 	import { contract, type TAccountInfo } from '$lib/web3';
-	import ModalSkeleton from './ModalSkeleton.svelte';
-	import { onClose } from '$lib/index';
+	import { web3 } from '$lib/web3';
+	import { X } from '@lucide/svelte';
 
 	interface INftCard {
 		nft: INft;
 		nftIdx: number;
 		account: TAccountInfo;
+		refreshNfts: () => void;
 	}
 
-	let { nft, nftIdx, account }: INftCard = $props();
+	let { nft, nftIdx, account, refreshNfts }: INftCard = $props();
 
-	let price: bigint = $state(0n);
-	let isOpen = $state(false);
-	let isOpenBuy = $state(false);
+	let price: string = $state('');
+	let nftImagePath: string | null = $state(null);
 
-	async function onPlaceNftOnSale(id: number) {
-		try {
-			const txHash = await contract.methods.placeNftOnSale(id, price).send({
-				from: account.address,
-				gas: '6721965'
-			});
+	let nftPlaceDialog: HTMLDialogElement | undefined = $state(undefined);
+	let nftBuyDialog: HTMLDialogElement | undefined = $state(undefined);
 
-			notificationsStore.add(
-				'success',
-				'Place NFT on sale',
-				`Nft on sale ${txHash.transactionHash}`
-			);
-		} catch (err) {
-			notificationsStore.add('error', 'Place NFT on sale', `Error: ${err}`);
-		} finally {
-			price = 0n;
-			isOpen = false;
+	function onOpenNftPlaceDialog() {
+		if (nftPlaceDialog === undefined) return;
+
+		if (!nftPlaceDialog.open) nftPlaceDialog.showModal();
+		else nftPlaceDialog.close();
+	}
+
+	function onOpenNftBuyDialog() {
+		if (nftBuyDialog === undefined) return;
+
+		if (!nftBuyDialog.open) nftBuyDialog.showModal();
+		else nftBuyDialog.close();
+	}
+
+	async function getNftImage() {
+		const resp = await fetch(`http://127.0.0.1:8080/api/nft/image/${nft.imageHash}`);
+
+		const data = await resp.json();
+
+		if (data) {
+			const lastPathname = String(data.path).split('/').slice(-1);
+			nftImagePath = `http://127.0.0.1:8080/uploads/${lastPathname}`;
 		}
 	}
 
-	async function buyNft(id: number) {
+	async function onPlace(id: number) {
+		try {
+			const txHash = await contract.methods
+				.placeNftOnSale(id, web3.utils.toWei(price, 'ether'))
+				.send({
+					from: account.address,
+					gas: '6721965'
+				});
+
+			notificationsStore.add(
+				'success',
+				'Place NFT on sale successfully',
+				`Tx hash: ${txHash.transactionHash}`
+			);
+
+			refreshNfts();
+		} catch (err) {
+			notificationsStore.add('error', 'Place NFT on sale error', `${err}`);
+		} finally {
+			price = '';
+			nftPlaceDialog?.close();
+		}
+	}
+
+	async function onBuy(id: number) {
 		try {
 			const txHash = await contract.methods.buyNft(id).send({
 				from: account.address,
@@ -49,50 +80,86 @@
 
 			notificationsStore.add(
 				'success',
-				'Buy NFT',
-				`Successfully buy NFT ${txHash.transactionHash}`
+				'Buy NFT successfully',
+				`Tx hash: ${txHash.transactionHash}`
 			);
+
+			refreshNfts();
 		} catch (err) {
-			notificationsStore.add('error', 'Buy NFT', `Error ${err}`);
+			notificationsStore.add('error', 'Buy NFT error', `${err}`);
+		} finally {
+			nftBuyDialog?.close();
 		}
 	}
+
+	$effect(() => {
+		getNftImage();
+	});
 </script>
 
 <div class="h-full w-96 rounded-xl border border-primary px-4 py-4">
 	<div>
-		<img src={nftIcon} alt="nft-icon" class=" h-[336px] w-full" />
+		{#if nftImagePath}
+			<img src={nftImagePath} aria-hidden="true" alt="NFT Image" class="h-[336px] w-full" />
+		{/if}
 	</div>
 	<div class="flex flex-col items-center gap-2">
 		<span class="text-2xl font-black text-white">{nft.name}</span>
+		<p class="break-words font-thin text-white">
+			Owner: <span class="text-sm font-bold">{nft.owner}</span>
+		</p>
+		<p class="text-left font-bold text-white">
+			Collectible: <span
+				class={`${nft.isCollectible ? 'text-collectableTrue' : 'text-collectableFalse'}`}
+				>{nft.isCollectible}</span
+			>
+		</p>
 		<span class="text-2xl font-black text-white">
 			{#if nft.saleData.isOnSale}
-				{nft.saleData.price} ETH
+				{web3.utils.fromWei(nft.saleData.price, 'ether')} ETH
 			{/if}
 		</span>
 		{#if nft.saleData.isOnSale && nft.owner !== account.address}
-			<Button onclick={() => (isOpenBuy = !isOpenBuy)} class="w-full">Buy NFT</Button>
+			<Button onclick={onOpenNftBuyDialog} class="w-full">Buy NFT</Button>
 		{:else if !nft.saleData.isOnSale && nft.owner === account.address}
-			<Button onclick={() => (isOpen = !isOpen)} class="w-full">Place on sale</Button>
+			<Button onclick={onOpenNftPlaceDialog} class="w-full">Place on sale</Button>
 		{/if}
 	</div>
 </div>
 
-<ModalSkeleton {isOpen} onClose={() => onClose(isOpen)}>
-	<div class="flex flex-col justify-center gap-3 text-2xl text-white">
-		<p>NFT: <span class="font-black">{nft.name}</span></p>
-		<p>OWNER: <span class="font-black">{nft.owner}</span></p>
+<dialog bind:this={nftPlaceDialog} class="rounded-md p-6">
+	<div class="relative">
+		<div class="absolute right-4 cursor-pointer">
+			<X onclick={() => nftPlaceDialog?.close()} />
+		</div>
+	</div>
+	<div class="flex flex-col gap-2">
+		<p class="text-2xl font-bold">{nft.name}</p>
+		<span class="text-xl font-thin">{nft.owner}</span>
+		<input
+			type="text"
+			placeholder="Price"
+			class="rounded-md border-2 px-2 py-2 outline-none focus:border-primary"
+			bind:value={price}
+		/>
+		<Button disabled={price === ''} onclick={() => onPlace(nftIdx)}>Place on sale</Button>
+	</div>
+</dialog>
+
+<dialog bind:this={nftBuyDialog} class="w-96 rounded-md p-6">
+	<div class="absolute right-4 cursor-pointer">
+		<X onclick={() => nftBuyDialog?.close()} />
 	</div>
 	<div class="flex flex-col gap-4">
-		<input type="text" placeholder="Price" bind:value={price} />
-		<Button onclick={() => onPlaceNftOnSale(nftIdx)}>Sale</Button>
+		<p class="text-2xl font-semibold">
+			NFT Name:
+			<span class="text-xl font-bold">{nft.name}</span>
+		</p>
+		<p class="text-2xl font-semibold">
+			Price: <span class="text-xl font-bold"
+				>{web3.utils.fromWei(nft.saleData.price, 'ether')} ETH</span
+			>
+		</p>
+		<Button onclick={() => onBuy(nftIdx)}>Buy</Button>
 	</div>
-</ModalSkeleton>
-
-<ModalSkeleton isOpen={isOpenBuy} onClose={() => onClose(isOpenBuy)}>
-	<div class="flex flex-col justify-center gap-3 text-2xl text-white">
-		<p>NFT: <span class="font-black">{nft.name}</span></p>
-		<p>OWNER: <span class="font-black">{nft.owner}</span></p>
-		<p>PRICE: {nft.saleData.price} ETH</p>
-	</div>
-	<Button onclick={() => buyNft(nftIdx)}>Buy</Button>
-</ModalSkeleton>
+</dialog>
