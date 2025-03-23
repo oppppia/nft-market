aebc187d20d01e21009cb88949987ec3c0eec5f3
elliotewpti <dmitriymarlowpierrebourne@gmail.com>
2025-03-24 00:57:39 +0300

    feat: Add refreshing collection

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,22 +9,21 @@
 	import CollectionList from '$components/CollectionList.svelte';
 	import { Api } from '$lib/api';
 	import TransactionList from '$components/TransactionList.svelte';
+	import type { ICollection, INft } from '$lib/icontract';
 
 	const accounts = getAccounts();
 	const tabs = ['Mint NFT', 'NFTs', 'Mint NFT collection', 'NFT Collections', 'Transactions'];
 	let activeTab = $state(tabs[0]);
 	let selectedAccount: TAccountInfo | undefined = $state(undefined);
+	let nfts: INft[] = $state([]);
+	let collections: ICollection[] = $state([]);
 
-	async function getNfts() {
-		const nfts = await contract.methods.getNfts().call();
-
-		return nfts;
+	async function refreshNfts() {
+		nfts = await contract.methods.getNfts().call();
 	}
 
-	async function getCollections() {
-		const collections = await contract.methods.getCollections().call();
-
-		return collections;
+	async function refreshCollections() {
+		collections = await contract.methods.getCollections().call();
 	}
 
 	async function getTransactions() {
@@ -40,6 +39,11 @@
 
 		return data.transactions;
 	}
+
+	$effect(() => {
+		refreshNfts();
+		refreshCollections();
+	});
 </script>
 
 <main class="flex min-h-screen flex-col gap-4 bg-main">
@@ -55,29 +59,23 @@
 	{#if activeTab === 'Mint NFT'}
 		<ArticleCard primaryName="MINT" secondaryName="NFT NOW"></ArticleCard>
 		{#if selectedAccount !== undefined}
-			<MintNft account={selectedAccount} />
+			<MintNft account={selectedAccount} {refreshNfts} />
 		{/if}
 	{:else if activeTab === 'NFTs'}
 		<ArticleCard primaryName="YOUR" secondaryName="NFTs"></ArticleCard>
 		{#if selectedAccount}
-			{#await getNfts()}
-				<span>Loading...</span>
-			{:then nfts}
-				<NftList {nfts} account={selectedAccount} />
-			{/await}
+			<NftList {nfts} account={selectedAccount} {refreshNfts} />
 		{/if}
 	{:else if activeTab === 'Mint NFT collection'}
 		<ArticleCard primaryName="MINT" secondaryName="NFT COLLECTION"></ArticleCard>
 		{#if selectedAccount !== undefined}
-			<MintCollection nfts={[]} account={selectedAccount} />
+			<MintCollection account={selectedAccount} {refreshCollections} />
 		{/if}
 	{:else if activeTab === 'NFT Collections'}
 		<ArticleCard primaryName="YOUR" secondaryName="NFTs COLLECTIONS"></ArticleCard>
-		{#await getCollections()}
-			<span>Loading collections...</span>
-		{:then collections}
-			<CollectionList {collections} />
-		{/await}
+		{#if selectedAccount}
+			<CollectionList {collections} {nfts} account={selectedAccount} {refreshNfts} />
+		{/if}
 	{:else if activeTab === 'Transactions'}
 		<ArticleCard primaryName="YOUR" secondaryName="TRANSACTIONS" />
 		{#await getTransactions()}
