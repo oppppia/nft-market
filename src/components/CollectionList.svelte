b5ae6ad929b7008a898e1bda40d5ac4bf2fa43fa
elliotewpti <dmitriymarlowpierrebourne@gmail.com>
2025-03-24 02:13:54 +0300

    feat(CollectionList): Add collections filter and improve UX/UI

--- src/components/CollectionList.svelte
+++ src/components/CollectionList.svelte
@@ -10,17 +10,36 @@
 		refreshNfts: () => void;
 	}
 
-	const { collections, nfts, account, refreshNfts }: ICollectionList = $props();
+	let { collections, nfts, account, refreshNfts }: ICollectionList = $props();
+
+	const tabs = ['My collections', 'Users collections'];
+	let activeTab = $state(tabs[0]);
+
+	const filteredCollections = $derived(
+		activeTab === tabs[0]
+			? collections.filter((collection) => collection.owner === account.address)
+			: collections.filter((collection) => collection.owner !== account.address)
+	);
 </script>
 
-<div class="flex items-center justify-center">
-	{#if collections.length > 0}
-		{#each collections as collection}
-			<div class="inline-grid grid-cols-3 gap-4">
+<div class="flex flex-col items-center justify-center">
+	<div class="flex items-center justify-center gap-5">
+		<select class="rounded-md px-12 py-2" bind:value={activeTab}>
+			{#each tabs as tab}
+				<option value={tab}>{tab}</option>
+			{/each}
+		</select>
+	</div>
+
+	{#if filteredCollections.length > 0}
+		<div class="inline-grid grid-cols-3 gap-4">
+			{#each filteredCollections as collection}
 				<CollectionCard {collection} {nfts} {account} {refreshNfts} />
-			</div>
-		{/each}
+			{/each}
+		</div>
 	{:else}
-		<span class="mx-auto text-2xl font-bold text-white">You haven't collections</span>
+		<span class="mx-auto text-2xl font-bold text-white">
+			{activeTab === tabs[0] ? "You don't have any collections" : 'No public collections available'}
+		</span>
 	{/if}
 </div>
