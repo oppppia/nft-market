c05dfd5a52787eb061efb6dbdfaf38b9e05c40ba
elliotewpti <dmitriymarlowpierrebourne@gmail.com>
2025-03-24 00:58:26 +0300

    fix(CollectionList): Add required props

--- src/components/CollectionList.svelte
+++ src/components/CollectionList.svelte
@@ -1,15 +1,23 @@
 <script lang="ts">
-	import type { ICollection } from '$lib/icontract';
+	import type { ICollection, INft } from '$lib/icontract';
+	import type { TAccountInfo } from '$lib/web3';
 	import CollectionCard from './CollectionCard.svelte';
 
-	const { collections }: { collections: ICollection[] } = $props();
+	interface ICollectionList {
+		collections: ICollection[];
+		nfts: INft[];
+		account: TAccountInfo;
+		refreshNfts: () => void;
+	}
+
+	const { collections, nfts, account, refreshNfts }: ICollectionList = $props();
 </script>
 
 <div class="flex items-center justify-center">
 	{#if collections.length > 0}
 		{#each collections as collection}
 			<div class="inline-grid grid-cols-3 gap-4">
-				<CollectionCard {collection} />
+				<CollectionCard {collection} {nfts} {account} {refreshNfts} />
 			</div>
 		{/each}
 	{:else}
